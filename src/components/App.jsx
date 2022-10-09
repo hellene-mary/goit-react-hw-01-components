9c1a94732bbedeeaef0d51ad12a97ad4576db1cf
Grek Maria <grekmariaa@gmail.com>
2022-10-09 21:18:34 +0300

    fix Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,13 @@ import transactions from '../data/transactions.json';
 export const App = () => {
     return (
         <>
-            <Profile user={user} />
+            <Profile
+                username={user.username}
+                tag={user.tag}
+                location={user.location}
+                avatar={user.avatar}
+                stats={user.stats}
+            />
             <Statistics stats={data} />
             <FriendList friends={friends} />
             <TransactionHistory transactions={transactions} />
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,7 @@
 import { PropTypes } from 'prop-types';
 import css from './Profile.module.css';
 
-export function Profile({ user }) {
-    const { avatar, username, tag, location, stats } = user;
-
+export function Profile({ avatar, username, tag, location, stats }) {
     return (
         <div className={css.profile}>
             <div className={css.description}>
@@ -32,15 +30,13 @@ export function Profile({ user }) {
 }
 
 Profile.propTypes = {
-    user: PropTypes.shape({
-        username: PropTypes.string.isRequired,
-        tag: PropTypes.string.isRequired,
-        location: PropTypes.string.isRequired,
-        avatar: PropTypes.string.isRequired,
-        stats: PropTypes.shape({
-            followers: PropTypes.number.isRequired,
-            views: PropTypes.number.isRequired,
-            likes: PropTypes.number.isRequired,
-        }),
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
     }),
 };
